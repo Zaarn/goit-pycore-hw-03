2aa5fa242e456d2f8ce291fb3ab48baba003ab09
Zaarn <stas.slada@gmail.com>
2026-02-13 20:38:46 +0200

    normalize_phone_fix

--- normalize_phone.py
+++ normalize_phone.py
@@ -5,19 +5,19 @@ def normalize_phone(phone_number):
     sanitized_numbers = []
     for number in phone_number:
         digits = re.sub(r"\D", "", number)
-        digits = re.sub(r"^[380+]{1,3}", "", digits)
+        digits = re.sub(r"^(380|38|0)", "", digits)
         if len(digits) == 9:
             digits = "+380" + digits
             sanitized_numbers.append(digits)
         else:
-            sanitized_numbers.append("Invalid number: " + number.strip())
+            sanitized_numbers.append("Invalid number: " + digits.strip())
     return sanitized_numbers
 
 phone_number = [
     "067\\t123 4567",
-    "(095) 234-5678\\n",
+    "(95) 234-5678\\n",
     "+380 44 123 4567",
-    "8050124567",
+    "83050 123 32 34",
     "    +38(050)123-32-34",
     "     0503451234",
     "(050)8889900",
